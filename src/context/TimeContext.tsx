b1fface62861b03f6c9346e9e5efcbc1d5f7c961
Christian Sanches <christian@endless-point.org>
2025-01-02 14:16:39 -0300

    ptbr minutes

--- src/context/TimeContext.tsx
+++ src/context/TimeContext.tsx
@@ -17,6 +17,16 @@ export const TimeProvider: React.FC<PropsWithChildren> = ({ children }) => {
 
   return (
     <TimeContext.Provider value={currentTime}>
+      {/* <select name="hour" id="hour" onChange={(e) => setCurrentTime(new Date(currentTime.setHours(parseInt(e.target.value))))}>
+        {[...Array(24).keys()].map((hour) => (
+          <option key={hour} value={hour}>{hour}</option>
+        ))}
+      </select>
+      <select name="minute" id="minute" onChange={(e) => setCurrentTime(new Date(currentTime.setMinutes(parseInt(e.target.value))))}>
+        {[...Array(60).keys()].map((minute) => (
+          <option key={minute} value={minute}>{minute}</option>
+        ))}
+      </select> */}
       {children}
     </TimeContext.Provider>
   );
